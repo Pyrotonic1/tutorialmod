9a6ac80f13abfe07b4f27e2c3e54bebeb42da0ac
Ben <benadrum12@gmail.com>
2025-10-21 16:36:03 -0400

    first version

---
+++ src/main/java/org/cibermo/tutorialmod/Tutorialmod.java
@@ -0,0 +1,10 @@
+package org.cibermo.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tutorialmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/cibermo/tutorialmod/client/TutorialmodClient.java
@@ -0,0 +1,10 @@
+package org.cibermo.tutorialmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
